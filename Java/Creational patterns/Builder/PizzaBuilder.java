9a74274754e4eca36adc22bdcff319d8572db681
MElHussieni <MahmoudMohamed.sw@cis.asu.edu.eg>
2018-07-06 09:47:42 +0200

    add Builder pattern

---
+++ Java/Creational patterns/Builder/PizzaBuilder.java
@@ -0,0 +1,15 @@
+public abstract class PizzaBuilder {
+    protected Pizza pizza;
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void createNewPizzaProduct() {
+        pizza = new Pizza();
+    }
+
+    public abstract void buildDough();
+    public abstract void buildSauce();
+    public abstract void buildTopping();
+}
